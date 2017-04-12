558be13d618988569dfa13e15fbc680a8db7fdf5
梁信众 <liangxinzhong@liangxinzhongdeMacBook-Pro.local>
2017-04-12 17:01:14 +0800

    implement groups#new

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,15 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+  def new
+    @group = Group.new
+  end
+  def create
+    @group = Group.new(group_params)
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
 end
