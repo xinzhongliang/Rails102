276bad87b433aa119209178d4add7427073bed4f
梁信众 <liangxinzhong@liangxinzhongdeMacBook-Pro.local>
2017-04-12 21:04:57 +0800

    install devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Hello World!'
+puts "这个种子档会自动建立一个帐号, 并且创建20个 groups, 每个 group 各 30 个 posts"
+
+create_account = User.create([email: 'example@gmail.com', password: '12345678', password_confirmation: '12345678'])
+
+create_groups = for i in 1..20 do
+  Group.create!([title: "Group no.#{i}", description: "这是用种子建立的第 #{i} 个讨论版", user_id: "1"])
+    for k in 1..30 do
+      Post.create!([group_id: "#{i}",content: "这是用种子建立的第 #{k} 个留言", user_id: "1"])
+  end
+end
